79c5c9ff03cfe52eb0d47914709a531a535133f9
Jammerz858 <Jammerz858@gmail.com>
2016-05-18 22:35:51 +0100

    (Core) Comments - LogBase Added comments to all properties of LogBase due... must of have too much sugar or something.

--- source/LogIt.Core/Log.cs
+++ source/LogIt.Core/Log.cs
@@ -9,18 +9,33 @@ namespace LogIt.Core
 {
     public class LogBase : ILoggable
     {
+        /// <summary>
+        /// The point in time the log message references. 
+        /// Usually the time the log was created.
+        /// </summary>
         public DateTime LogTime { get; set; }
 
+        /// <summary>
+        /// Specifies the type or severity of the log. 
+        /// Allows for log read and write filtering
+        /// </summary>
         public LoggingType LogType { get; set; }
 
+        /// <summary>
+        /// The primary message of the log. 
+        /// Usually summarised to allow for quick judgement of log severity.
+        /// </summary>
         public string Message { get; set; }
 
+        /// <summary>
+        /// For all log detail that does not belong in or would pollute the primary log message.
+        /// </summary>
         public NameValueCollection Detail { get; set; }
 
         /// <summary>
         /// Base implementation of an ILoggable instance. 
         /// For use in high volume logging scenarios where the overhead of 
-        /// variable assingments and initialisations could become costly.
+        /// variable assignments and initialisations could become costly.
         /// For scenarios less performance sensitive use or inherit Log.
         /// </summary>
         public LogBase()
@@ -39,7 +54,11 @@ namespace LogIt.Core
             // **************************************************************************
             // **************************************************************************
         }
-        
+
+        /// <summary>
+        /// Return LogBase implementation of instance.
+        /// </summary>
+        /// <returns>LogBase implementation of instance.</returns>
         public LogBase GetLog()
         {
             return this;
@@ -72,6 +91,7 @@ namespace LogIt.Core
             Message = message ?? String.Empty;
             LogType = logType;
             Detail = new NameValueCollection(DEFAULT_KEY_COMPARISON) { logDetail ?? new NameValueCollection() };
+            base.GetLog();
         }
     }
 }
