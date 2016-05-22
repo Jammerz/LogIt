1cf898ba7fffdf02369bb23de6d284e63e851e47
Jammerz858 <Jammerz858@gmail.com>
2016-05-22 19:14:26 +0100

    (Core) ILogWriter Alterations Added Identifier, Name and Config properties to ILogWriter.

--- source/LogIt.Core/ILogWriter.cs
+++ source/LogIt.Core/ILogWriter.cs
@@ -8,6 +8,12 @@ namespace LogIt.Core
 {
     public interface ILogWriter : ILogConnection
     {
+        Guid Identifier { get; }
+
+        string Name { get; }
+
+        LogConfig Config { get; set; }
+
         Log Write(LogBase log);
 
         IEnumerable<Log> Write(List<LogBase> logs);
